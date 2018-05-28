a59cf5f0bb2e6b98103da22c79c7fb9767c4c82d
Frederic Hamel <frederic.hamel123@gmail.com>
2018-05-27 23:41:08 -0400

    Improve build.sh script

--- build.sh
+++ build.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/bash
+set -e
 
-BUILD_LIB=".builds/v4.8.9@C/_build.o1"
-if [[ -f $BUILD_LIB ]];then
-  echo Use old build
-  gsc-script -i -e '(define-macro (macro-load-build) `(load "lib/_build"))' _build-bootstrap.scm
+SCRIPT_NAME=$(realpath $0)
+SCRIPT_PATH=${SCRIPT_NAME%/*}
+
+BUILD_DIR="$SCRIPT_PATH/.builds/v4.8.9@C"
+LIBNAME="_build.o1"
+
+cd $SCRIPT_PATH
+if [[ -f lib/$LIBNAME ]];then
+  echo "Using lib/_build"
+  gsc-script -i -e '(load "lib/_build")(define-macro (macro-load-build) `(##require-module _build))' _build-bootstrap.scm
 else
-  echo Create bootstrap
+  echo "Building bootstrap"
   gsc-script -i -e '(define-macro (macro-load-build) `(load "_build"))' _build-bootstrap.scm
 fi
 test -d lib || mkdir lib
-cp $BUILD_LIB lib
+cp $BUILD_DIR/$LIBNAME lib
+cd -
